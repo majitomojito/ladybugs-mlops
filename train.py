e01402b5d6c66c52cfe6636614d85cc1644ac2a1
majitomojito <mahita.ajmera@gmail.com>
2026-03-22 14:39:36 +0100

    cleaning up and logging

--- train.py
+++ train.py
@@ -9,4 +9,9 @@ sys.path.insert(0, str(Path(__file__).parent))
 from src.pipeline import run_pipeline
 
 if __name__ == "__main__":
+    import logging
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
     run_pipeline()
